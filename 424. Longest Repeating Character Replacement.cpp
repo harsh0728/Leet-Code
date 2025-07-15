9784cd0c4ac51408ac5a438316733c72d97cf155
Harsh Kumar <103038137+harsh0728@users.noreply.github.com>
2025-07-15 19:34:52 +0530

    Replaced hashmap with frequency array for better performance

--- 424. Longest Repeating Character Replacement.cpp
+++ 424. Longest Repeating Character Replacement.cpp
@@ -7,12 +7,19 @@ public:
         unordered_map<char,int> mp;
         while (right<s.length())
         {
-            mp[s[right]]++;
-            max_count=max(max_count,mp[s[right]]);
+            // using map
+            //mp[s[right]]++;
+            //max_count=max(max_count,mp[s[right]]);
+            
+            // using vector
+            freq[s[right]-'A']++;
+            max_count=max(max_count,freq[s[right]-'A']);
+            
             int window_size=right-left+1;
             if (window_size-max_count>k)
             {
-                mp[s[left]]--;
+                //mp[s[left]]--; // using map
+                freq[s[left]-'A']--; // using vector
                 left++;
             }
             res=max(res,right-left+1);
